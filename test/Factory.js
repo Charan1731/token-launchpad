2bd83ab4517fe1b22e8590f7c9783b8eb5d14a31
Charandeep Reddy K <charan23114@gmail.com>
2025-06-24 21:59:15 +0530

    Initialised token

--- test/Factory.js
+++ test/Factory.js
@@ -2,14 +2,54 @@ const { loadFixture } = require("@nomicfoundation/hardhat-toolbox/network-helper
 const { expect } = require("chai")
 const { ethers } = require("hardhat")
 
-describe("Factory", function () {
-    it("should have a name", async function(){
+describe("Factory",  () => {
+    const FEE = ethers.parseUnits("0.01",18)
+
+    //deploying the contract by creating an abstract contract
+    const deployFactoryFixture = async () => {
+
+        const [deployer, creator] = await ethers.getSigners();
+
         const Factory = await ethers.getContractFactory("Factory");
 
-        const factory = await Factory.deploy();
 
-        const name = await factory.name();
+        const factory = await Factory.deploy(FEE);
 
-        expect(name).to.equal("Factory");
+        const transaction = await factory.connect(creator).create("COINflop", "COIN", { value:FEE });
+        await transaction.wait();
+
+        const tokenAddress = await factory.tokens(0);
+
+        const Token = await ethers.getContractAt("Token", tokenAddress);
+
+        return { factory, deployer, creator, token: Token }
+    }
+
+    describe("Deployment", () => {
+        it("should set the fee", async () => {
+            const { factory} = await deployFactoryFixture();
+
+            const fee = await factory.fee();
+
+            expect(fee).to.equal(FEE);
+        })
+
+        it("should set the owner", async () => {
+            const {factory, deployer} = await deployFactoryFixture();
+
+            const owner = await factory.owner();
+
+            expect(owner).to.equal(deployer.address);
+        })
+    })
+
+    describe("Create", () => {
+        it("should set the owner", async () => {
+            const { factory, token } = await deployFactoryFixture();
+
+            const owner = await token.owner();
+
+            expect(owner).to.equal( await factory.getAddress());
+        })
     })
 })
